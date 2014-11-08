86b0d6599de19c90b07ebb90b67c2d1733b005fc
Jivko Rusev <JR@Jivkos-MacBook-Air.local>
2014-11-08 02:52:02 +0200

    changed userproifle

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -5,17 +5,15 @@ var profile = function(req, res) {
     var user = req.user;
     // req.authInfo is set using the `info` argument supplied by `BearerStrategy`.
     // It is typically used to indicate scope of the token, and used in access control checks. 
+    // e.g. req.authInfo.scope 
     console.log(user);
     res.send({ 
-            user: {
-                userId: user.userId,
-                username: user.username,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                imageUrl: user.imageUrl
-            },
-            scope: req.authInfo.scope 
-        });
+        userId: user.userId,
+        username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        imageUrl: user.imageUrl
+    });
 };
 
 var getAllUsers = function(req, res) {
