37cdf343c869e30b19cf3d8ea76366956318d767
jrusev <jrusev@gmail.com>
2014-11-10 17:08:44 +0200

    added mongolab

--- itemEx/server/models/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var UserModel = require('../models/user'),
-    MessageModel = require('../models/message');
-
-module.exports = {
-    User : UserModel,
-    Message: MessageModel
-};
